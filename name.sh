2800c02dd345e01de291fce4f4d831998936dde0
Ubuntu <ubuntu@base01.classic>
2025-12-31 17:40:00 +0000

    to chech jenkins is running are not

---
+++ name.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+service = "jenkins"
+if $service is_active --quite; then
+	echo "the service is running"
+	systemctl status jenkins
+else;
+	systemctl start jenkins
+	systemctl status jenkins
+fi
